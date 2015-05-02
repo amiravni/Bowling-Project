8c07cdad0596f8bb36b2d3082e44eb93fa7f2935
Danny Wainshtein <danny.wainshtein@gmail.com>
2015-05-02 18:09:06 +0300

    fixed a delay problem when using USB cameras, now there is a thread that capturing the images and the capture function just takes the last photo using mutex so we wouldn't copy a while setting a new image

--- hardware/cameras/usb_camera.py
+++ hardware/cameras/usb_camera.py
@@ -1,8 +1,13 @@
 import time
 import io 
 import cv2
+from threading import Thread, Lock
+from thread import start_new_thread
+
 
 camera = None
+current_photo = None
+current_photo_lock = Lock()
 
 def init(res,shutter_speed):
     global camera
@@ -11,13 +16,30 @@ def init(res,shutter_speed):
     camera.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, res[0])
     camera.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, res[1])
     camera.set(cv2.cv.CV_CAP_PROP_EXPOSURE,exposure_time)
+    start_new_thread(infinite_capture_photos,())
 
+
+def infinite_capture_photos():
+    global current_photo
+    global current_photo_lock
+    while True:
+        current_photo_lock.acquire()
+        try:
+            s,current_photo = camera.read()
+        finally:
+            current_photo_lock.release()
+        time.sleep(0.03)
 
 def capture_image(name):
     if camera is None:
         return
 
-    s, img_rgb = camera.read()
+    #s, img_rgb = camera.read()
+    current_photo_lock.acquire()
+    try:
+        img_rgb = current_photo 
+    finally:
+        current_photo_lock.release()
     cv2.imwrite(name,img_rgb)
 
 
